d09cc31f634994a5493dfc718d5938cfade12b1f
CharlieDoo <dchen518@163.com>
2026-04-02 16:29:54 +0800

    fix: correct import path for add_selfloops and eliminate_selfloops

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,8 @@ from src.model import SiGNN
 from src.layers import TALayer, Aggregator
 from src.neuron import BLIF, reset_net
 from src.datasets import Dataset, DBLP, Tmall, Patent
-from src.sampling import Sampler, RandomWalkSampler
-from src.utils import set_seed, tab_printer, add_selfloops, eliminate_selfloops
+from src.sampling import Sampler, RandomWalkSampler, add_selfloops, eliminate_selfloops
+from src.utils import set_seed, tab_printer
 
 __all__ = [
     "SiGNN",
